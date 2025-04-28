c8361a6b3b91ec369ed192dc0ecd35c15f2d27b4
theoldelon <130331456+theoldelon@users.noreply.github.com>
2025-04-28 22:03:59 +0800

    Update list.blade.php
    Changed Default Text to "No Payment Transaction Yet"

--- resources/views/admin/payments/list.blade.php
+++ resources/views/admin/payments/list.blade.php
@@ -114,7 +114,7 @@
                                             @endforeach
                                         @else
                                             <tr>
-                                                <td colspan="10" class="text-center text-danger">Payment Transaction Not Found!</td>
+                                                <td colspan="10" class="text-center text-danger">No Payment Transaction Yet!</td>
                                             </tr>
                                         @endif
                                     </tbody> 
